c52c8ad4a0039dade5905cc4790fecadc7f99073
andrew <andrew.r224@gmail.com>
2021-06-28 13:08:39 +0300

    Add user sign in form :smiling_imp:

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -15,17 +15,25 @@ export const createCache = ()=> {
   return cache;
 };
 
-
 // getToken from meta tags
-const getToken = () =>
-      document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-const token = getToken();
-const setTokenForOperation = async operation =>
-      operation.setContext({
-        headers: {
-          'X-CSRF-Token': token,
-        },
-      });
+const getTokens = () => {
+  const tokens = {
+    "X-CSRF-Token": document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute("content")
+  };
+  const authToken = localStorage.getItem("mlToken");
+  return authToken ? { ...tokens, Authorization: authToken } : tokens;
+};
+
+const setTokenForOperation = async operation => {
+  return operation.setContext({
+    headers: {
+      ...getTokens()
+    }
+  });
+};
+
 // link with token
 const createLinkWithToken = () =>
       new ApolloLink(
