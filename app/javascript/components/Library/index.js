d99b591771efd2464d746b40a0490ef001e83d45
andrew <andrew.r224@gmail.com>
2021-07-05 19:45:35 +0300

    Add Update Item form :scream_cat:

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Query } from "react-apollo";
 import { LibraryQuery } from "./operations.graphql";
+import UpdateItemForm from "../UpdateItemForm";
 import cs from "./styles";
 
 const Library = () => {
@@ -10,21 +11,32 @@ const Library = () => {
       {({ data, loading }) => (
         <div className={cs.library}>
           {loading || !data.items
-            ? "loading..."
-            : data.items.map(({ title, id, user, imageUrl, description }) => (
-                <button
-                  key={id}
-                  className={cs.plate}
-                  onClick={() => setItem({ title, imageUrl, id, description })}
-                >
-                  <div className={cs.title}>{title}</div>
-                  <div>{description}</div>
-                  {imageUrl && <img src={imageUrl} className={cs.image} />}
-                  {user ? (
-                    <div className={cs.user}>added by {user.email}</div>
-                  ) : null}
-                </button>
-              ))}
+           ? "loading..."
+           : data.items.map(({ title, id, user, imageUrl, description }) => (
+             <button
+               key={id}
+               className={cs.plate}
+               onClick={() => setItem({ title, imageUrl, id, description })}
+             >
+               <div className={cs.title}>{title}</div>
+               <div>{description}</div>
+               {imageUrl && <img src={imageUrl} className={cs.image} />}
+               {user ? (
+                 <div className={cs.user}>added by {user.email}</div>
+               ) : null}
+             </button>
+           ))}
+          {
+            item !== null && (
+              <UpdateItemForm
+                id={item.id}
+                initialTitle={item.title}
+                initialDescription={item.description}
+                initialImageUrl={item.imageUrl}
+                onClose={() => setItem(null)}
+              />
+            )
+          }
         </div>
       )}
     </Query>
